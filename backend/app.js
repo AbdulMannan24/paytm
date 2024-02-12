4bee56c8c1c6d30bc5c9cfb7d40557404ac0dfb0
Mohd Abdul Mannan <abdulmannan@Mohds-MacBook-Air.local>
2024-02-12 08:14:17 +0530

    DB connection created

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const connectDB = require("./db");
 const app = express();
 const port = process.env.PORT || 3000;
 
+require('dotenv').config();
 
+app.get('/', (req, res)=> {
+    res.send("Api Server running");
+})
 
 
 app.listen(port, ()=> {
     console.log("Server Started: " + port);
+    connectDB();
 });
