9a4da128b0e3f86f6f7eb3aff81a403abff31483
Mohd Abdul Mannan <ma24042000@gmail.com>
2024-04-16 18:29:04 +0530

    bug fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import SingUp from './pages/SingUp'
 import SignIn from './pages/SignIn'
 import Dashboard from './pages/Dashboard'
